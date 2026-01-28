c39af9e5b59c8bf5799b0d32848f08c14eb4b363
Joshua Defredes <mrjoshgm@gmail.com>
2026-01-28 15:24:39 +1100

    Added different pages to the website and linked the pages to the respective nav bar buttons

--- my-portfolio/app/components/NavBar.tsx
+++ my-portfolio/app/components/NavBar.tsx
@@ -1,23 +1,40 @@
-import ThemeToggle from "./ThemeToggle"
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
 
-function NavBar(){
-    return(
-        <>
-            <nav className="flex flex-row items-center justify-between px-[50px] py-[25px] mx-[250px] my-[0px]">
-                <div className="text-[20px] font-[500] py-[5px] px-[10px] cursor-default select-none">
-                    Joshua Defredes
-                </div>
-                    <ul className="flex items-center justify-center flex-row gap-[10px]">
-                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Home</li>
-                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Projects</li>
-                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">About Me</li>
-                        <ThemeToggle />
-                    </ul> 
-            </nav>
+function NavBar() {
+  return (
+    <>
+      <nav className="flex flex-row items-center justify-between px-[50px] py-[25px] mx-[250px] my-[0px]">
+        <Link href="/" className="text-[20px] font-[500] py-[5px] px-[10px] cursor-pointer select-none">
+          Joshua Defredes
+        </Link>
+        
+        <ul className="flex items-center justify-center flex-row gap-[10px]">
+          <li className="list-none hover:bg-(--base-variant) rounded-[50px] cursor-pointer">
+            <Link href="/" className="inline-block text-[20px] px-[10px] py-[5px]">
+              Home
+            </Link>
+          </li>
 
-            <hr className="block h-[1px] rounded-[1px] mx-[200px]" />
-        </>
-    )
+          <li className="list-none hover:bg-(--base-variant) rounded-[50px] cursor-pointer">
+            <Link href="/projects" className="inline-block text-[20px] px-[10px] py-[5px]">
+              Projects
+            </Link>
+          </li>
+
+          <li className="list-none hover:bg-(--base-variant) rounded-[50px] cursor-pointer">
+            <Link href="/about" className="inline-block text-[20px] px-[10px] py-[5px]">
+              About Me
+            </Link>
+          </li>
+          
+          <ThemeToggle />
+        </ul>
+      </nav>
+
+      <hr className="block h-[1px] rounded-[1px] mx-[200px] mb-[5vh]" />
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <main className="">
+      <h1>Welcome Home!</h1>
     </main>
   );
 }
