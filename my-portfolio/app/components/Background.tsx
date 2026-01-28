7eba4f960a5df52a8848e0a39c43b05ac760a6d4
Joshua Defredes <mrjoshgm@gmail.com>
2026-01-28 12:52:45 +1100

    Removed background, need to fix later. Adjusted Nav bar design.

--- my-portfolio/app/components/Background.tsx
+++ my-portfolio/app/components/Background.tsx
@@ -1,6 +1,6 @@
 function Background(){
     return(
-        <div className="absolute h-[500px] w-[1000px] bg-(--accent-color) rounded-[50%] m-auto top-[-400px] left-0 right-0 blur-[350px] z-[-1]"></div>
+        <div></div>
     )
 }
 
--- my-portfolio/app/components/NavBar.tsx
+++ my-portfolio/app/components/NavBar.tsx
@@ -4,18 +4,18 @@ function NavBar(){
     return(
         <>
             <nav className="flex flex-row items-center justify-between px-[50px] py-[25px] mx-[250px] my-[0px]">
-                <div className="text-[40px] font-[500] py-[5px] px-[10px] cursor-default select-none">
+                <div className="text-[20px] font-[500] py-[5px] px-[10px] cursor-default select-none">
                     Joshua Defredes
                 </div>
                     <ul className="flex items-center justify-center flex-row gap-[10px]">
-                        <li className="list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Home</li>
-                        <li className="list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Projects</li>
-                        <li className="list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">About Me</li>
+                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Home</li>
+                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">Projects</li>
+                        <li className="text-[20px] list-none px-[10px] rounded-[50px] py-[5px] hover:bg-(--base-variant) hover:cursor-pointer">About Me</li>
                         <ThemeToggle />
                     </ul> 
             </nav>
 
-            <hr className="block h-[1px] rounded-[10%] mx-[250px]" />
+            <hr className="block h-[1px] rounded-[1px] mx-[200px]" />
         </>
     )
 }
